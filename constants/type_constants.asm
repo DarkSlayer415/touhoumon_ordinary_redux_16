5f7a633d472268d812ea226bc58e24ff09b657e4
DarkSlayer415 <thedarkslayer415@gmail.com>
2026-09-20 22:00:30 +0900

    Readd Fairy type and change type properties.
    Readd Fairy type, change Ghost and Poison to Special types, and change Dark to Physical type.

--- constants/type_constants.asm
+++ constants/type_constants.asm
@@ -10,16 +10,15 @@ DEF PHYSICAL EQU const_value
 	const NORMAL
 	const FIGHTING
 	const FLYING
-	const POISON
 	const GROUND
 	const ROCK
 	const BIRD
 	const BUG
-	const GHOST
+	const DARK
 	const STEEL
 
 DEF UNUSED_TYPES EQU const_value
-	const_next 19
+	const_next 18
 	const CURSE_TYPE
 DEF UNUSED_TYPES_END EQU const_value
 
@@ -28,10 +27,12 @@ DEF SPECIAL EQU const_value
 	const WATER
 	const GRASS
 	const ELECTRIC
+	const POISON
 	const PSYCHIC_TYPE
 	const ICE
+	const GHOST
 	const DRAGON
-	const DARK
+	const FAIRY
 DEF TYPES_END EQU const_value
 
 DEF NUM_TYPES EQU TYPES_END + UNUSED_TYPES - UNUSED_TYPES_END - 1 ; discount BIRD
